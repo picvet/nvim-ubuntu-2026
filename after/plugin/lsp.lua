58c1f237e899dfd52ec0a8a4667a22661c9d0144
Percy Zulu <percyzulu2@outlook.com>
2026-04-25 16:58:22 +0200

    feat: add pyright to lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,12 +9,11 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
 	-- Replace the language servers listed here
 	-- with the ones you want to install
-	ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd'},
+	ensure_installed = {'lua_ls', 'rust_analyzer', 'clangd', 'pyright'},
 	handlers = {
 		function(server_name)
 			require('lspconfig')[server_name].setup({})
 		end,
 	}
 })
-
 
